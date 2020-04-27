01f6289993ca4da9d793a063bee60e6ac5414011
Romarioagrow <romarioagrow@mail.ru>
2020-04-27 18:08:43 +0500

    Spring MVC + AngularJS

--- src/main/java/app/repos/ProductRepo.java
+++ src/main/java/app/repos/ProductRepo.java
@@ -1,4 +1,5 @@
 package app.repos;
 
 public interface ProductRepo {
+    /**/
 }
---
+++ src/main/resources/sql/create-table.sql
@@ -0,0 +1,8 @@
+/*CREATE TABLE `user_detail` (
+  `productID` int(10) NOT NULL AUTO_INCREMENT,
+  `productName` varchar(20) NOT NULL,
+  `productType` varchar(15) NOT NULL,
+  `productInfo` varchar(100) NOT NULL,
+  `productPrice` BIGINT(16) NOT NULL,
+  PRIMARY KEY (`productID`)
+);*/
