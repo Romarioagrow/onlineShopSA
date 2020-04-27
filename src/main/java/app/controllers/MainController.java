043af8a3931a5a4730a8a31fe812381d4a07dce2
Romarioagrow <romarioagrow@mail.ru>
2020-04-27 18:10:01 +0500

    Spring MVC + AngularJS

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,14 +1,16 @@
 package app.controllers;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Log
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String mainMethod() {
-        System.out.println("INDEX GET");
+        log.info("GET INDEX");
         return "static/index.html";
     }
 
